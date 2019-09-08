337903bf0a022d27e88b7bd777cd3200bf207e77
2vlad19880508 <54082201+2vlad19880508@users.noreply.github.com>
2019-09-08 23:16:39 +0300

    Update profile.php

--- 13/profile.php
+++ 13/profile.php
@@ -5,37 +5,27 @@ echo 'Ваше ник ' . $_SESSION['admin'] . '<br>';
 echo 'Ваше мейл ' . $_SESSION['mail'] . '<br>';
 echo 'Ваш пароль ' . $_SESSION['pass'] . '<br>';
 echo 'Ваше имя ' . $_SESSION['name'] . '<br>';
-?>
+
+if ($_POST['name'] == null) {
+    echo 'Укажите имя!';
+} elseif ($_POST['user'] == null) {
+	echo 'Укажите ник!';
+} elseif ($_POST['mail'] == null) {
+	echo 'Укажите mail!';
+} elseif ($_POST['pass'] == null) {
+	echo 'Укажите Пароль!';
+} else {   
+	$_SESSION['admin'] = $_POST['user'];
+	$_SESSION['mail'] = $_POST['mail'];
+	$_SESSION['pass'] = $_POST['pass'];
+	$_SESSION['name'] = $_POST['name']; ?>
+<meta http-equiv="refresh" content="0; url=login.php">
+<?php } ?>
 <h3>Изменить данные</h3>
-<?php 
-if($_POST) 
-{	
-	if ($_POST['name'] == null) 
-	{
-        echo 'Укажите имя!';
-	} elseif ($_POST['user'] == null) 
-	{
-		echo 'Укажите ник!';
-	} elseif ($_POST['mail'] == null) 
-	{
-		echo 'Укажите mail!';
-	} elseif ($_POST['pass'] == null) 
-	{
-		echo 'Укажите Пароль!';
-    } else {   
-		session_start();
-		$_SESSION['admin'] = $_POST['user'];
-		$_SESSION['mail'] = $_POST['mail'];
-		$_SESSION['pass'] = $_POST['pass'];
-		$_SESSION['name'] = $_POST['name'];
-		header("Location: login.php");
-	}
-}
-?>
-<form method="post" action="form.php">
+<form method="post" action="profile.php">
  Имя: <input type="text" name="name" /><br />
  Ник: <input type="text" name="user" /><br />
  мейл: <input type="text" name="mail" /><br />
  Пароль: <input type="password" name="pass" /><br />
- <input type="submit" name="submit" value="Войти" />
+ <input type="submit" name="submit" value="изменить" />
 </form>
