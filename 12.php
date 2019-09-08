e74aa710c4be0636e8f91db9b68b9fa22740596e
2vlad19880508 <54082201+2vlad19880508@users.noreply.github.com>
2019-09-08 22:27:29 +0300

    Update 12.php

--- 12.php
+++ 12.php
@@ -1,28 +1,23 @@
 <?php	
-if ($_POST['name'] == null) {
-    echo 'Укажите имя!';
-} else {    
-	// если нет, то счетчик 0 и создаем куки
-	if (!isset($_COOKIE['name'])) {		
-		$name = $_POST['name'];	
-		setcookie("names", $name, 0x6FFFFFFF);			
-	}
-}
-    
-
-
-if (isset($_COOKIE['count'])) 
-{ 	// проверяем есть ли куки 
+// проверяем есть ли куки 
+if (isset($_COOKIE['count'])) {
 	$cnt = $_COOKIE['count']+1; // если да, то +1 к счетчику
 } else {
-	// если нет, то счетчик 0 и создаем куки
-	$cnt=0;
+	$cnt=0; // если нет, то счетчик 0 и создаем куки
 }
-	setcookie("count", $cnt, 0x6FFFFFFF);?>
+	setcookie("count", $cnt, 0x6FFFFFFF);
 	
+if ($_POST['name'] == null) {
+    echo 'Укажите имя!'; // если поле пустое, пишет укажите имя
+} else {    
+	$name = $_POST['name'];	
+	setcookie("name", $name, 0x6FFFFFFF);			
+}
+	
+?>
 	
 <p>Вы посещали эту страницу <b> <?php echo $_COOKIE['count'] ?> </b> раз</p>
 <form action="12.php" method="POST" name="formMain" id="formMain">
-  Ваше имя: <input type = "text" value = "<?php echo $_COOKIE['names']; ?>" name = "name" ><br>
-  <input type="submit" value="Отправить">
- </form> 
+	Ваше имя: <input type = "text" value = "<?php echo $_COOKIE['name']; ?>" name = "name" ><br>
+	<input type="submit" value="Отправить">
+</form> 
