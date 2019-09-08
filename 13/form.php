c57f4d08abcfbedb4fbca8b099d05e8b700ed788
2vlad19880508 <54082201+2vlad19880508@users.noreply.github.com>
2019-09-08 23:15:56 +0300

    Update form.php

--- 13/form.php
+++ 13/form.php
@@ -1,26 +1,19 @@
-<?php 
-if($_POST) 
-{	
-	if ($_POST['name'] == null) 
-	{
-        echo 'Укажите имя!';
-	} elseif ($_POST['user'] == null) 
-	{
-		echo 'Укажите ник!';
-	} elseif ($_POST['mail'] == null) 
-	{
-		echo 'Укажите mail!';
-	} elseif ($_POST['pass'] == null) 
-	{
-		echo 'Укажите Пароль!';
-    } else {   
-		session_start();
-		$_SESSION['admin'] = $_POST['user'];
-		$_SESSION['mail'] = $_POST['mail'];
-		$_SESSION['pass'] = $_POST['pass'];
-		$_SESSION['name'] = $_POST['name'];
-		header("Location: login.php");
-	}
+<?php 	
+if ($_POST['name'] == null) {
+    echo 'Укажите имя!';
+} elseif ($_POST['user'] == null) {
+	echo 'Укажите ник!';
+} elseif ($_POST['mail'] == null) {
+	echo 'Укажите mail!';
+} elseif ($_POST['pass'] == null) {
+	echo 'Укажите Пароль!';
+} else {   
+	session_start();
+	$_SESSION['admin'] = $_POST['user'];
+	$_SESSION['mail'] = $_POST['mail'];
+	$_SESSION['pass'] = $_POST['pass'];
+	$_SESSION['name'] = $_POST['name'];
+	header("Location: login.php");
 }
 ?>
 <form method="post" action="form.php">
